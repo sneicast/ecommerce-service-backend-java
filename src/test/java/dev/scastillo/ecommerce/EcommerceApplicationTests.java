3d63330ced076b1f1df9bbe5712e40504ce6d30c
Sneider Castillo <sneiderz15@gmail.com>
2025-07-31 11:15:11 -0500

    commit initial

---
+++ src/test/java/dev/scastillo/ecommerce/EcommerceApplicationTests.java
@@ -0,0 +1,15 @@
+package dev.scastillo.ecommerce;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.ActiveProfiles;
+
+@SpringBootTest
+@ActiveProfiles("test")
+class EcommerceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
